fcf4637124622e828436e398af26518034dc3fd6
macie <macie@DESKTOP-QVTEEJ2>
2020-09-13 11:01:23 +0100

    Dodaj pliki projektów.

---
+++ gra_euklidesa.cpp
@@ -0,0 +1,32 @@
+﻿// gra_euklidesa.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
+//
+
+#include <iostream>
+
+int main()
+{
+    int t, a, b;
+
+    std::cin >> t;
+    for (int i = 0; i < t; i++)
+    {
+        std::cin >> a >> b;
+        while (true)
+        {
+            if (a == b)
+            {
+                std::cout << a + b << std::endl;
+                break;
+            }
+            if (a < b)
+            {
+                b -= a;
+            }
+            if (a > b)
+            {
+                a -= b;
+            }
+        }
+        }
+
+}
